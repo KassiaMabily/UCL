1df0d9f02f74ee6e0fe88c32cae25a47e583ffb8
Kassia Fraga <kassiafraga7@gmail.com>
2021-08-24 13:50:49 -0300

    Novos exercícios

---
+++ Algoritmos e Estrutura de Dados I/URI Online Judge/Outros/1038/1038/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entered = Console.ReadLine().Split(null);
+            int codigo = int.Parse(entered[0]);
+            int quantidade = int.Parse(entered[1]);
+
+            double total = 0.0;
+
+            switch (codigo)
+            {
+                case 1:
+                    total = quantidade * 4.00;
+                    break;
+                case 2:
+                    total = quantidade * 4.50;
+                    break;
+                case 3:
+                    total = quantidade * 5.00;
+                    break;
+                case 4:
+                    total = quantidade * 2.00;
+                    break;
+                case 5:
+                    total = quantidade * 1.50;
+                    break;
+                default:
+                    total = 0;
+                    break;
+            }
+
+            Console.WriteLine("Total: R$ {0:0.00}", total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Algoritmos e Estrutura de Dados I/URI Online Judge/Outros/1040/1040/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entered = Console.ReadLine().Split(null);
+
+            float n1 = float.Parse(entered[0], CultureInfo.InvariantCulture);
+            float n2 = float.Parse(entered[1], CultureInfo.InvariantCulture);
+            float n3 = float.Parse(entered[2], CultureInfo.InvariantCulture);
+            float n4 = float.Parse(entered[3], CultureInfo.InvariantCulture);
+
+            float media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Media: " + Math.Round(media, 1).ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (media >= 5.0 && media <= 6.9)
+            {
+                float exame = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Media: " + Math.Round(media, 1).ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno em exame.");
+
+                Console.WriteLine("Nota do exame: " + Math.Round(exame, 1).ToString("F1", CultureInfo.InvariantCulture));
+                media = (media + exame) / 2;
+                if (media >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+
+                Console.WriteLine("Media final: " + Math.Round(media, 1).ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else if (media < 5.0)
+            {
+                Console.WriteLine("Media: " + Math.Round(media, 1).ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno reprovado.");
+            }
+        }
+    }
+}
