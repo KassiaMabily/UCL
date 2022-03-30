56e3443a367d358d99627cff5894dc4bb23e1ba4
Kassia Fraga <kassiafraga7@gmail.com>
2022-03-30 19:42:54 -0300

    Exercicio da aula

---
+++ 3º Período/Algoritmos e Estrutura de Dados II/MaiorDe4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MaiorDe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4];
+            for(int counter = 0; counter < 4; counter++)
+            {
+                numbers[counter] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Format("Maior numero eh: {0}", numbers.Max()));
+        }
+    }
+}
